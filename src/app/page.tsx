361dabb95b043eecd48b223fb142ef686a029886
Jawad Ali <162344230+JawadShoukat@users.noreply.github.com>
2024-10-18 23:19:54 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h1">
+
+      <h1>Hello World</h1>
+
+      <p>Next js Hello World create by Jawad Shoukat</p>
+      
+    </div>
+   
+  );
+}
+
+
